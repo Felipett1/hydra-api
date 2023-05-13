71df5f9785dea60843e56c95a085487ae44c7197
Felipe Triviño <felipetriv@hotmail.com>
2023-05-12 20:37:18 -0500

    Felipe Triviño: Se carga ajuste de API para cambiar el metodo get por post.

--- router/pago.js
+++ router/pago.js
@@ -3,10 +3,10 @@ const controller = require("../controllers/pago")
 const router = express.Router()
 const path = '/pago'
 
-router.get(path, controller.consultarSubContratoDetalle)
+router.post(path, controller.consultarSubContratoDetalle)
 router.get(path + '/cliente', controller.consultarCliente)
 router.get(path + '/subcontrato', controller.consultarSubContrato)
-router.post(path, controller.cargar)
+//router.post(path, controller.cargar)
 router.put(path + '/valor', controller.modificarPago)
 router.post(path + '/masivo', controller.cargarMasivo)
 module.exports = router
